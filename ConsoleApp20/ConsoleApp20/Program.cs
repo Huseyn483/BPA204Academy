3ac55b4c2cb6b7eae6d397025ce34d8692608433
Huseyn483 <huseynli2006huseyn@gmail.com>
2026-03-03 17:23:50 +0400

    4cu task

---
+++ ConsoleApp20/ConsoleApp20/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp20;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		int a = 200;
+		int b = 100;
+		int c = 300;
+		int d = 150;
+		int cem = a + b + c + d;
+		int orta = cem / 4;
+		Console.WriteLine(cem + " " + orta);
+	}
+}
